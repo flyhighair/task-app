72b782080c2a3837e755fc06342e1a6c64036d74
flyhighair <flyhighup.air25@gmail.com>
2019-05-10 06:37:58 +0900

    Add task detail view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,8 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.paginate(page: params[:page], per_page: 10)
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
 end
